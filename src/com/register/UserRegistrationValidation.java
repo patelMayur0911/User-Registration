12b68f6fcce882d190c3eaeec3dcd61755282131
Patel_Mayur <patelmayuraaa@gmail.com>
2023-12-27 15:32:02 +0530

    Uc 2 - last name

--- src/com/register/UserRegistrationValidation.java
+++ src/com/register/UserRegistrationValidation.java
@@ -8,11 +8,16 @@ public class UserRegistrationValidation {
 
         Scanner scannerValue = new Scanner(System.in);
         System.out.println("Enter First Name : ");
-//        First name starts with Cap and has minimum 3 characters
+
+//        UC1 First name starts with Cap and has minimum 3 characters
         String firstName = scannerValue.next();
         String regex1 = "[A-Z]{1}[a-z]{3,}";
         System.out.println(" Validation : " + firstName.matches(regex1));
 
+//        UC 2 Last name starts with Cap and has  minimum 3 characters
+        String lastName = scannerValue.next();
+        String regex2 = "[A-Z]{1}[a-z]{3,}";
+        System.out.println(" Validation : " + lastName.matches(regex2));
 
     }
 }
